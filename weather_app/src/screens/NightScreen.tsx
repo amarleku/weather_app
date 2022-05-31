814250f7339dae0c1f20ee38067b1567e04518f0
Astrit Marleku <astrit.marleku@lhind.dlh.de>
2022-05-31 23:20:35 +0200

    Started to work on Night Screen

---
+++ weather_app/src/screens/NightScreen.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const NightScreen: React.FC = () => {
+    return (
+        <div>
+            <h1>Night Screen</h1>
+        </div>
+    );
+};
+
+export default NightScreen;
+
