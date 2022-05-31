282b0eb6e39e98b4efb3a84d50bc8d3b96e8477b
Astrit Marleku <astrit.marleku@lhind.dlh.de>
2022-05-31 23:24:17 +0200

    Working on Night Screen

--- weather_app/src/screens/NightScreen.tsx
+++ weather_app/src/screens/NightScreen.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
+import Search from "../components/SearchBar/Search";
+
+import { ResponseData } from "../components/SearchBar/constants";
+import axios from "axios";
 
 
 const NightScreen: React.FC = () => {
+
+    const [initialData, setInitialData] = useState<ResponseData>({
+        name: '',
+        days: [
+            {
+                temp: '',
+                description: '',
+                humidity: '',
+                windSpeed: ''
+            }
+        ],
+        errorMsg: ''
+    });
     return (
         <div>
             <h1>Night Screen</h1>
