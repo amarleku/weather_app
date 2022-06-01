cdec7e2b32a3afde608bd924b2e0ca9dc5ffc269
U761842 <vilma.tirana@lhind.dlh.de>
2022-06-01 13:50:32 +0200

    Rain Screen added

--- weather_app/src/components/SearchBar/BackgroundComponent.tsx
+++ weather_app/src/components/SearchBar/BackgroundComponent.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import sunSVG  from '../../assets/sun.svg';
 import moonSVG from '../../assets/moon.svg';
 import snowSVG from '../../assets/snow.svg';
+import rainSVG from '../../assets/rain.svg';
 
 const MainComponent:React.FC = () => {
     return(
@@ -10,7 +11,8 @@ const MainComponent:React.FC = () => {
             <div className="weatherLogo">
                 {/* <img src={sunSVG} alt="Sun SVG" className="sunSVG" /> */}
                 {/* <img src={moonSVG} alt="Moon SVG" className="moonSVG" /> */}
-                <img src={snowSVG} alt="Moon SVG" className="moonSVG" />
+                {/*<img src={snowSVG} alt="Moon SVG" className="moonSVG" />*/}
+                <img src={rainSVG} alt="Rain SVG" className="rainSVG" />
             </div>
         </div>
     );
