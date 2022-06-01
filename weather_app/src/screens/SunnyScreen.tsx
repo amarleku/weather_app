fb3ea83dbeb802def54dd66a093df83ed1c7a263
NENE <kristi.nene@lhind.dlh.de>
2022-06-01 12:25:04 +0200

    feature/ Added 5 day forecast & error messages and default city

--- weather_app/src/screens/SunnyScreen.tsx
+++ weather_app/src/screens/SunnyScreen.tsx
@@ -10,24 +10,24 @@ import axios from "axios";
 import BackgroundComponent from "../components/SearchBar/BackgroundComponent";
 
 const SunnyScreen:React.FC = () => {
+    
+    const[location, setLocation] = useState<string>('');
+    
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&include=days&key=T8F4NJ3HKJVN9BQNVJLVMSGJE&contentType=json`; 
 
-    const [initialData, setInitialData] = useState<ResponseData>({
-        name: '',
-        days: [
-            {
-                temp: '',
-                description: '',
-                humidity: '',
-                windSpeed: ''
-            }
-        ],
-        errorMsg: ''
-    });
+    const defaultURL = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Tirana?unitGroup=metric&include=days&key=T8F4NJ3HKJVN9BQNVJLVMSGJE&contentType=json';
+
+    useEffect(() => {
+        axios.get(defaultURL).then((response: any) => {
+            setData(response.data);
+        });
+    }, []);
 
     const[data,setData] = useState<ResponseData>({
-        name: '',
+        address: '',
         days: [
-            {
+            {   
+                datetime: '',
                 temp: '',
                 description: '',
                 humidity: '',
@@ -37,36 +37,17 @@ const SunnyScreen:React.FC = () => {
         errorMsg: ''
     });
 
-    const onLoadGetRequestUrl = `https://community-open-weather-map.p.rapidapi.com/forecast/daily`;
-
-
-    const[location, setLocation] = useState<string>('');
 
     const[errorMsg, setErrorMsg] = useState<string>('');
 
-    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&include=days&key=T8F4NJ3HKJVN9BQNVJLVMSGJE&contentType=json`;
 
     const searchLocation = (event: { key: string; }) => {
         if(event.key===("Enter")) {
             axios.get(url).then((response: any) => {
                 setData(response.data);
-                setInitialData({
-                    name: '',
-                    days: [
-                        {
-                            temp: '',
-                            description: '',
-                            humidity: '',
-                            windSpeed: ''
-                        }
-                    ],
-                    errorMsg: ''
-                })
                 setErrorMsg('');
-                console.log(response.data);
             }).catch((error) => {
                 setErrorMsg(error.response.data);
-                console.log(error.response.data);
             });
         }
     }
@@ -78,65 +59,20 @@ const SunnyScreen:React.FC = () => {
             <div className="card custom-card">
                 <div className="card-body">
                     <div className="inputWrapper">
-                        {/*<input type="text" className="form-control searchInput" placeholder="Search" />*/}
                         <Search searchLocation={(event) => searchLocation(event)}
                                 handleInputChange={(event) => setLocation(event.target.value)}/>
                     </div>
-                    {/* Mock Weather Info of how forcast can be */}
                     <div className="infoWrapper">
-                        <div>
-                            <div className="stats">
-                                <h2>Tirana, Albania</h2>
-                                <h3>13°C</h3>
-                                <h3>Sunny</h3>
-                            </div>
-                            <div className="otherStats pt-5 pb-3">
-                                <h3>Humidity: 40%</h3>
-                                <h3>Wind Speed: 10km/h</h3>
-                            </div>
-                        </div>
-                        <div>
-                            <div className="stats">
-                                <h2>Tirana, Albania</h2>
-                                <h3>13°C</h3>
-                                <h3>Sunny</h3>
-                            </div>
-                            <div className="otherStats pt-5 pb-3">
-                                <h3>Humidity: 40%</h3>
-                                <h3>Wind Speed: 10km/h</h3>
-                            </div>
-                            <div className="pt-2">
-                                <h3>{errorMsg}</h3>
-                            </div>
-                        </div>
-                        <div>
-                            <div className="stats">
-                                <h2>Tirana, Albania</h2>
-                                <h3>13°C</h3>
-                                <h3>Sunny</h3>
-                            </div>
-                            <div className="otherStats pt-5 pb-3">
-                                <h3>Humidity: 40%</h3>
-                                <h3>Wind Speed: 10km/h</h3>
-                            </div>
-                            <div className="pt-2">
-                                <h3>{errorMsg}</h3>
-                            </div>
-                        </div>
-                        <div>
-                            <div className="stats">
-                                <h2>Tirana, Albania</h2>
-                                <h3>13°C</h3>
-                                <h3>Sunny</h3>
-                            </div>
-                            <div className="otherStats pt-5 pb-3">
-                                <h3>Humidity: 40%</h3>
-                                <h3>Wind Speed: 10km/h</h3>
-                            </div>
-                            <div className="pt-2">
-                                <h3>{errorMsg}</h3>
-                            </div>
-                        </div>
+                        {errorMsg == '' ? data.days.slice(0, 5).map((item, index) => (
+                                <div className="stats" key={index}>
+                                    <h2 className="date-header">{ item.datetime }</h2>
+                                    <h3 className="pt-4">{ data.address }</h3>
+                                    <h3> { item.temp }°C</h3>
+                                    <h3> { item.description } </h3>
+                                    <h3 className="pt-4">Humidity: { item.humidity }%</h3>
+                                    <h3>Wind Speed: { item.windSpeed }</h3>
+                                </div>
+                        )): <div><h2>{ errorMsg }</h2></div>}
                     </div>
                 </div>
             </div>
