433bed2385582549d9c6a7df90c27eb0d9d010b3
Astrit Marleku <astrit.marleku@lhind.dlh.de>
2022-06-01 09:44:17 +0200

    Create Snow Component with all the things inside

--- weather_app/src/components/SearchBar/BackgroundComponent.tsx
+++ weather_app/src/components/SearchBar/BackgroundComponent.tsx
@@ -2,13 +2,15 @@ import React from "react";
 // Import SVG Logo
 import sunSVG  from '../../assets/sun.svg';
 import moonSVG from '../../assets/moon.svg';
+import snowSVG from '../../assets/snow.svg';
 
 const MainComponent:React.FC = () => {
     return(
         <div className="background">
             <div className="weatherLogo">
                 {/* <img src={sunSVG} alt="Sun SVG" className="sunSVG" /> */}
-                <img src={moonSVG} alt="Moon SVG" className="moonSVG" />
+                {/* <img src={moonSVG} alt="Moon SVG" className="moonSVG" /> */}
+                <img src={snowSVG} alt="Moon SVG" className="moonSVG" />
             </div>
         </div>
     );
