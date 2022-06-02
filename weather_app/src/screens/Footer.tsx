0fea36bc763483557ff3fe7383eee93d8f35e6a0
Astrit Marleku <astrit.marleku@lhind.dlh.de>
2022-06-02 12:35:47 +0200

    Make Date Dynamic

--- weather_app/src/screens/Footer.tsx
+++ weather_app/src/screens/Footer.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import './styles/footer.css'
+import React from 'react';import './styles/footer.css'
 
 interface Props { }
 
 const Footer: React.FC = () => {
 
   let date = new Date();
+  
   return (
     <>
       <footer className="footer">
-      <p className='text-center footer-text'>Copyright Weather 2022</p>
+      <p className='text-center footer-text'>Copyright Weather {`${date.getFullYear()}`}</p>
       </footer>
       
       </>)
